0efd6d4f7b98fe0bba584f0d79a02f3c1bf78841
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-01-06 02:08:18 +0000

    refactor: remove core-js

--- src/compose.ts
+++ src/compose.ts
@@ -1,23 +1,31 @@
 import { IHooks, IAsyncHooks } from './types'
 import { isntUndefined } from '@blackglory/types'
-import 'core-js/features/array/flat'
+import flatten from 'lodash.flatten'
 
 export function compose<T>(...hooks: Array<IHooks<T>>): IHooks<T>
 export function compose<T>(...hooks: Array<IAsyncHooks<T>>): IAsyncHooks<T>
 export function compose<T>(...hooks: Array<IHooks<T> | IAsyncHooks<T>>): IAsyncHooks<T>
 export function compose<T>(...hooks: Array<IHooks<T> | IAsyncHooks<T>>) {
   return {
-    before: hooks.map(x => x.before)
-                 .filter(isntUndefined)
-                 .flat()
-  , returning: hooks.map(x => x.returning)
-                    .filter(isntUndefined)
-                    .flat()
-  , throwing: hooks.map(x => x.throwing)
-                   .filter(isntUndefined)
-                   .flat()
-  , finally: hooks.map(x => x.finally)
-                  .filter(isntUndefined)
-                  .flat()
+    before: flatten(
+      hooks
+        .map(x => x.before)
+        .filter(isntUndefined)
+    )
+  , returning: flatten(
+      hooks
+        .map(x => x.returning)
+        .filter(isntUndefined)
+    )
+  , throwing: flatten(
+      hooks
+        .map(x => x.throwing)
+        .filter(isntUndefined)
+    )
+  , finally: flatten(
+      hooks
+        .map(x => x.finally)
+        .filter(isntUndefined)
+    )
   }
 }
